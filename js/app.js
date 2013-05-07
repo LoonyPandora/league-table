360d9f954051d654e61652e32b70cc7ef5d3e634
James Aitken <loonypandora@gmail.com>
2013-05-07 15:57:09 +0100

    Create one big data structure of dates to iterate over

--- js/app.js
+++ js/app.js
@@ -5,7 +5,9 @@
 // NOT doing this may improve performance, but it would be a premature
 // optimization at this stage with such a trivial app and no performance issues
 window.onload = function () {
-    // calculateDates();
+    // iterateDays();
+
+    var ISODates = createDateRange();
 
     // Get the games into a useful data structure. An object keyed by date seems sensible
     var gamesByDate = {};
@@ -25,7 +27,11 @@ window.onload = function () {
         gamesByDate[gameDateISO].push(game);
     });
 
-    console.log(gamesByDate);
+
+    for (var i = 0; i < ISODates.length; i++) {
+        console.log(ISODates[i]);
+    };
+
 
 }
 
@@ -44,7 +50,7 @@ function loadGames(g) {
 // Date math is HARD - I would never do. this in real life
 // See this great talk for reasons why it's silly to actually use this code
 // http://youtu.be/OhjOXrFHL7o
-function calculateDates() {
+function iterateDays() {
 
     // Length of each month in days - hardcoded 29 days in Feb 2012 since it was a leap year
     // I don't need all of them, but it makes the code mode readable.
@@ -54,7 +60,7 @@ function calculateDates() {
     for (var i = 0; i < monthNames.length; i++) {
         // Changing the length of months to be zero-indexed could be confusing, I think this is more readable
         for (var j = 0; j < monthLengths[i]; j++) {
-            // console.log(j + 1, getOrdinal(j + 1), monthNames[i]);
+            console.log(j + 1, getOrdinal(j + 1), monthNames[i]);
         };
     };
 }
@@ -79,3 +85,36 @@ function getOrdinal(number) {
         return "th";
     }
 }
+
+
+
+// Gives us a nice list of ISO dates for 2011 and 2012
+// It's a larger range than we need, but the code is simpler and more readable this way.
+function createDateRange() {
+    var years = [2011, 2012];
+    var monthLengths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    var ISODates = [];
+    // Nested loops aren't cool...
+    for (var i = 0; i < years.length; i++) {
+        for (var j = 0; j < monthLengths.length; j++) {
+            for (var x = 0; x < monthLengths[j]; x++) {
+                ISODates.push(years[i] + "-" + pad(j + 1) + "-" + pad(x + 1));
+            };
+            
+        };
+    };
+
+    return ISODates;
+}
+
+
+// Pads single digits to 2 digits long with leading zeros
+function pad(number) {
+    var str = "" + number;
+    while (str.length < 2) {
+        str = "0" + str;
+    }
+    return str;
+};
+
