3a7b06bccd447cc694ccbaea29849119c4826158
James Aitken <loonypandora@gmail.com>
2013-05-07 14:08:26 +0100

    Add in barebones JS

---
+++ js/app.js
@@ -0,0 +1,9 @@
+// Trivial app, but it's always good to be strict
+"use strict";
+
+// All DOM stuff should wait until we have a DOM
+// NOT doing this may improve performance, but it would be a premature
+// optimization at this stage with such a trivial app and no performance issues
+window.onload = function () {
+
+}
