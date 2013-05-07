593571ef88f055fc51f8665d2e3a3fa996bc3bdd
James Aitken <loonypandora@gmail.com>
2013-05-07 16:19:20 +0100

    Cleanup and some more data structure messing around
    We now have an object with every date in 2011-2012, and an array of matches played on that day.

--- js/app.js
+++ js/app.js
@@ -5,33 +5,14 @@
 // NOT doing this may improve performance, but it would be a premature
 // optimization at this stage with such a trivial app and no performance issues
 window.onload = function () {
-    // iterateDays();
-
     var ISODates = createDateRange();
 
-    // Get the games into a useful data structure. An object keyed by date seems sensible
-    var gamesByDate = {};
-
-    // Only modern browsers need apply
-    games.map(function(game) {
-
-        // A bit hairy. Clean this up
-        var gameDateISO = game.date.split("/").reverse();
-        gameDateISO[0] = 20 + gameDateISO[0];
-        gameDateISO = gameDateISO.join("-");
-
-        if (!gamesByDate[gameDateISO]) {
-            gamesByDate[gameDateISO] = [];
-        }
-
-        gamesByDate[gameDateISO].push(game);
+    // May as well use the fast modern tools we have available if we don't support old browsers
+    Object.keys(games).forEach(function(key) {
+        ISODates[convertToISO8601(games[key].date)].push(games[key]);
     });
 
-
-    for (var i = 0; i < ISODates.length; i++) {
-        console.log(ISODates[i]);
-    };
-
+    console.log(ISODates);
 
 }
 
@@ -44,26 +25,6 @@ function loadTeams(t) {
 function loadGames(g) {
     games = g;
 };
-
-
-
-// Date math is HARD - I would never do. this in real life
-// See this great talk for reasons why it's silly to actually use this code
-// http://youtu.be/OhjOXrFHL7o
-function iterateDays() {
-
-    // Length of each month in days - hardcoded 29 days in Feb 2012 since it was a leap year
-    // I don't need all of them, but it makes the code mode readable.
-    var monthLengths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-    for (var i = 0; i < monthNames.length; i++) {
-        // Changing the length of months to be zero-indexed could be confusing, I think this is more readable
-        for (var j = 0; j < monthLengths[i]; j++) {
-            console.log(j + 1, getOrdinal(j + 1), monthNames[i]);
-        };
-    };
-}
 
 
 
@@ -88,24 +49,39 @@ function getOrdinal(number) {
 
 
 
+// Date math is HARD - I would never do. this in real life
+// See this great talk for reasons why it's silly to actually use this code
+// http://youtu.be/OhjOXrFHL7o
+
 // Gives us a nice list of ISO dates for 2011 and 2012
 // It's a larger range than we need, but the code is simpler and more readable this way.
 function createDateRange() {
     var years = [2011, 2012];
     var monthLengths = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-    var ISODates = [];
+    var ISODates = {};
     // Nested loops aren't cool...
     for (var i = 0; i < years.length; i++) {
         for (var j = 0; j < monthLengths.length; j++) {
             for (var x = 0; x < monthLengths[j]; x++) {
-                ISODates.push(years[i] + "-" + pad(j + 1) + "-" + pad(x + 1));
+                ISODates[years[i] + "-" + pad(j + 1) + "-" + pad(x + 1)] = [];
             };
             
         };
     };
 
     return ISODates;
+}
+
+
+function convertToISO8601(date) {
+    // A bit hairy. Clean this up
+    var ISODate = date.split("/").reverse();
+    ISODate[0] = 20 + ISODate[0];
+    ISODate[1] = pad(ISODate[1]);
+    ISODate[2] = pad(ISODate[2]);
+
+    return ISODate.join("-");
 }
 
 
